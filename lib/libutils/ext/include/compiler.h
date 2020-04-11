d4a5d9377466a0b10f9fc21ee59b0ec7950f90cd
Hernan Gatta <hegatta@microsoft.com>
2020-04-11 00:24:51 -0700

    libutils: implement fallthrough macro
    Instead of commenting /* FALLTRHOUGH */, introduce the fallthrough macro,
    using GCC's built-in fallthrough attribute if supported (since 7.1) and in
    Clang since at least 4.0.0. Falls back onto an empty statement if
    unsupported.
    
    This makes checkpatch happy, too, which checks fallthrough comments as of
    February 2020.
    
    Signed-off-by: Hernan Gatta <hegatta@microsoft.com>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- lib/libutils/ext/include/compiler.h
+++ lib/libutils/ext/include/compiler.h
@@ -70,6 +70,12 @@
 #define __GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + \
 		       __GNUC_PATCHLEVEL__)
 
+#if __GCC_VERSION >= 70100
+#define fallthrough	__attribute__((fallthrough))
+#else
+#define fallthrough	do {} while (0)  /* fallthrough */
+#endif
+
 #if __GCC_VERSION >= 50100 && !defined(__CHECKER__)
 #define __HAVE_BUILTIN_OVERFLOW 1
 #endif
